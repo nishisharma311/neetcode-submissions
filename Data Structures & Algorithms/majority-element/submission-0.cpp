55cec2cc103a19546be926cf63ef5236e9f7f023
Nishi Sharma <u19ee035@eed.svnit.ac.in>
2026-05-15 16:38:19 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int ans;
+        for(int i: nums) {
+            mp[i]++;
+        }
+        for(auto i:mp) {
+            if(i.second > nums.size()/2)
+                ans = i.first;
+        }
+        return ans;
+    }
+};
